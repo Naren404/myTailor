729706d91b996d9ee3d1c51283afddfe4879b34e
Naren404 <narendrasunar47@gmail.com>
2023-07-09 12:10:32 +1000

    modal form added to upload measurements for order

--- app/components/ItemCard/index.tsx
+++ app/components/ItemCard/index.tsx
@@ -1,13 +1,18 @@
-import { ItemModel } from "~/models/item.server";
+import { useState } from "react";
+import { useLoaderData } from "@remix-run/react";
+
+import OrderModal from "../OrderModal";
 
 interface ItemCardProps {
-  item: ItemModel
+  item: typeof useLoaderData<'item'>
 }
 
 const ItemCard = (props: ItemCardProps) => {
-  const { item: { name, imageUrl } } = props
+  const { item, item: { name, imageUrl } } = props
   
+  const [showModal, setState] = useState(false)
   return ( 
+    <>
       <div className="group rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
         <div className="relative overflow-hidden rounded-3xl">
           <img src={imageUrl} alt="" loading="lazy" width="1000" height="667" className="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
@@ -17,11 +22,15 @@ const ItemCard = (props: ItemCardProps) => {
           <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
             {name}
           </h3>
-          <a className="inline-block" href="#">
+          <button className="inline-block" onClick={ ()=> setState(true) }>
             <span className="text-info dark:text-blue-300">Select This Design</span>
-          </a>
+          </button>
         </div>
       </div>
+
+      {/* Place Order Modal */}
+      {showModal && <OrderModal item={item} setState={setState} />}
+    </>
    )
 }
  
